ed80c0e71e632878e2a1cb549ccdea51d12d791a
mkamiishi <mkamiishi@infragistics.com>
2024-06-24 15:04:27 +0900

    初回コミット
    Initial commit

---
+++ UltraDockManager_Reset/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace UltraDockManager_Reset
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ultraDockManager1 = new Infragistics.Win.UltraWinDock.UltraDockManager(this.components);
+            this._Form1UnpinnedTabAreaLeft = new Infragistics.Win.UltraWinDock.UnpinnedTabArea();
+            this._Form1UnpinnedTabAreaRight = new Infragistics.Win.UltraWinDock.UnpinnedTabArea();
+            this._Form1UnpinnedTabAreaTop = new Infragistics.Win.UltraWinDock.UnpinnedTabArea();
+            this._Form1UnpinnedTabAreaBottom = new Infragistics.Win.UltraWinDock.UnpinnedTabArea();
+            this._Form1AutoHideControl = new Infragistics.Win.UltraWinDock.AutoHideControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.ultraDockManager1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ultraDockManager1
+            // 
+            this.ultraDockManager1.CompressUnpinnedTabs = false;
+            this.ultraDockManager1.HostControl = this;
+            // 
+            // _Form1UnpinnedTabAreaLeft
+            // 
+            this._Form1UnpinnedTabAreaLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this._Form1UnpinnedTabAreaLeft.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this._Form1UnpinnedTabAreaLeft.Location = new System.Drawing.Point(0, 0);
+            this._Form1UnpinnedTabAreaLeft.Name = "_Form1UnpinnedTabAreaLeft";
+            this._Form1UnpinnedTabAreaLeft.Owner = this.ultraDockManager1;
+            this._Form1UnpinnedTabAreaLeft.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this._Form1UnpinnedTabAreaLeft.Size = new System.Drawing.Size(0, 450);
+            this._Form1UnpinnedTabAreaLeft.TabIndex = 8;
+            // 
+            // _Form1UnpinnedTabAreaRight
+            // 
+            this._Form1UnpinnedTabAreaRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this._Form1UnpinnedTabAreaRight.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this._Form1UnpinnedTabAreaRight.Location = new System.Drawing.Point(800, 0);
+            this._Form1UnpinnedTabAreaRight.Name = "_Form1UnpinnedTabAreaRight";
+            this._Form1UnpinnedTabAreaRight.Owner = this.ultraDockManager1;
+            this._Form1UnpinnedTabAreaRight.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this._Form1UnpinnedTabAreaRight.Size = new System.Drawing.Size(0, 450);
+            this._Form1UnpinnedTabAreaRight.TabIndex = 9;
+            // 
+            // _Form1UnpinnedTabAreaTop
+            // 
+            this._Form1UnpinnedTabAreaTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this._Form1UnpinnedTabAreaTop.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this._Form1UnpinnedTabAreaTop.Location = new System.Drawing.Point(0, 0);
+            this._Form1UnpinnedTabAreaTop.Name = "_Form1UnpinnedTabAreaTop";
+            this._Form1UnpinnedTabAreaTop.Owner = this.ultraDockManager1;
+            this._Form1UnpinnedTabAreaTop.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this._Form1UnpinnedTabAreaTop.Size = new System.Drawing.Size(800, 0);
+            this._Form1UnpinnedTabAreaTop.TabIndex = 10;
+            // 
+            // _Form1UnpinnedTabAreaBottom
+            // 
+            this._Form1UnpinnedTabAreaBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this._Form1UnpinnedTabAreaBottom.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this._Form1UnpinnedTabAreaBottom.Location = new System.Drawing.Point(0, 450);
+            this._Form1UnpinnedTabAreaBottom.Name = "_Form1UnpinnedTabAreaBottom";
+            this._Form1UnpinnedTabAreaBottom.Owner = this.ultraDockManager1;
+            this._Form1UnpinnedTabAreaBottom.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this._Form1UnpinnedTabAreaBottom.Size = new System.Drawing.Size(800, 0);
+            this._Form1UnpinnedTabAreaBottom.TabIndex = 11;
+            // 
+            // _Form1AutoHideControl
+            // 
+            this._Form1AutoHideControl.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this._Form1AutoHideControl.Location = new System.Drawing.Point(0, 0);
+            this._Form1AutoHideControl.Name = "_Form1AutoHideControl";
+            this._Form1AutoHideControl.Owner = this.ultraDockManager1;
+            this._Form1AutoHideControl.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this._Form1AutoHideControl.Size = new System.Drawing.Size(0, 0);
+            this._Form1AutoHideControl.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(72, 302);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 15;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.Location = new System.Drawing.Point(337, 286);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(187, 100);
+            this.panel2.TabIndex = 16;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel3.Location = new System.Drawing.Point(569, 286);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 100);
+            this.panel3.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this._Form1AutoHideControl);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this._Form1UnpinnedTabAreaBottom);
+            this.Controls.Add(this._Form1UnpinnedTabAreaTop);
+            this.Controls.Add(this._Form1UnpinnedTabAreaRight);
+            this.Controls.Add(this._Form1UnpinnedTabAreaLeft);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ultraDockManager1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Infragistics.Win.UltraWinDock.UltraDockManager ultraDockManager1;
+        private Infragistics.Win.UltraWinDock.AutoHideControl _Form1AutoHideControl;
+        private Infragistics.Win.UltraWinDock.UnpinnedTabArea _Form1UnpinnedTabAreaBottom;
+        private Infragistics.Win.UltraWinDock.UnpinnedTabArea _Form1UnpinnedTabAreaTop;
+        private Infragistics.Win.UltraWinDock.UnpinnedTabArea _Form1UnpinnedTabAreaRight;
+        private Infragistics.Win.UltraWinDock.UnpinnedTabArea _Form1UnpinnedTabAreaLeft;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ UltraDockManager_Reset/Form1.cs
@@ -0,0 +1,49 @@
+﻿using Infragistics.Win.UltraWinDock;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UltraDockManager_Reset
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+			SuspendLayout();
+			DockableControlPane panel1View =
+			  new DockableControlPane("panel1", "パネル1", this.panel1);
+			DockableControlPane panel2View =
+			  new DockableControlPane("panel2", "パネル2", this.panel2);
+			DockableControlPane panel3View =
+			  new DockableControlPane("panel3", "パネル3", this.panel3);
+
+			DockAreaPane dapLeft = new DockAreaPane(DockedLocation.DockedLeft);
+			dapLeft.Panes.Add(panel1View);
+			DockAreaPane dapBottom = new DockAreaPane(DockedLocation.DockedBottom);
+			dapBottom.Panes.Add(panel2View);
+			DockAreaPane dapRight = new DockAreaPane(DockedLocation.DockedRight);
+			dapRight.Panes.Add(panel3View);
+
+			this.ultraDockManager1.DockAreas.Add(dapLeft);
+			this.ultraDockManager1.DockAreas.Add(dapBottom);
+			this.ultraDockManager1.DockAreas.Add(dapRight);
+			ResumeLayout();
+
+			this.ultraDockManager1.SaveComponentSettings();
+		}
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+			this.ultraDockManager1.LoadComponentSettings();
+		}
+    }
+}
